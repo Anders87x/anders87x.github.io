19250c10b4d8ee7fc13d80df6904817d9ab6aafd
Anderson <davis_anderson_87@hotmail.com>
2020-07-12 16:14:23 -0500

    12/07/2020 Correcion de problema en el blog

--- js/main.js
+++ js/main.js
@@ -219,25 +219,4 @@
         });
     }
 
-    //Leaflet Map
-
-    var mymap = L.map('map').setView([40.6700, -73.9400], 14);
-
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-        maxZoom: 18,
-        attribution: '',
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1
-    }).addTo(mymap);
-
-    var icon = L.divIcon({
-        className: 'custom-div-icon',
-        html: "<div class='map-marker'></div>",
-        iconSize: [30, 30],
-        iconAnchor: [15, 15]
-    });
-
-    L.marker([40.6700, -73.9400], { icon: icon }).addTo(mymap);
-
 })(jQuery);
